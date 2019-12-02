93a39b7ed007e506366f184de138cd0783ea67ba
Shady <Shadymtadros@hotmail.com>
2019-12-02 15:48:48 +0100

    added validaions

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates_presence_of :first_name, :last_name, :email, :password
+  validates_uniqueness_of :email
 end
