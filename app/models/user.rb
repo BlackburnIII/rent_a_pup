9fddd420781950526ba5016836799032b8da114c
James McLaren III <james.mclaren.3rd@gmail.com>
2019-12-02 16:43:22 +0100

    add dependent destroy and has many'

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates_presence_of :first_name, :last_name, :email, :encrypted_password
   validates_uniqueness_of :email
+  has_many :puppies, dependent: :destroy
+  has_many :bookings, dependent: :destroy
+  has_many :reviews, through: :bookings
 end
