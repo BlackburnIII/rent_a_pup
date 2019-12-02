fdc3082a2cbb048e533c74ccdfd5fe746700eb7c
James McLaren III <james.mclaren.3rd@gmail.com>
2019-12-02 14:46:44 +0100

    added bookings table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_134312) do
+ActiveRecord::Schema.define(version: 2019_12_02_134550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "user_id"
+    t.bigint "puppy_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["puppy_id"], name: "index_bookings_on_puppy_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "puppies", force: :cascade do |t|
     t.string "name"
@@ -41,5 +52,7 @@ ActiveRecord::Schema.define(version: 2019_12_02_134312) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "puppies"
+  add_foreign_key "bookings", "users"
   add_foreign_key "puppies", "users"
 end
