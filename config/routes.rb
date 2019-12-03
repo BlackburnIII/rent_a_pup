dd7279f8e7e97cc06223f458d22331de4394d97a
Beatrice Corti <beshorts@mail.com>
2019-12-03 20:54:28 +0100

    some changes on home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   resources :puppies
-
-
   devise_for :users
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
