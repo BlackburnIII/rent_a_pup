4d47bd6529c6a0aeefdb80e7fe65cf8f43e7fc8b
James McLaren III <james.mclaren.3rd@gmail.com>
2019-12-04 14:11:10 +0100

    added create account and fixed logic on home if

--- db/migrate/20191204113701_remove_user_from_review.rb
+++ db/migrate/20191204113701_remove_user_from_review.rb
@@ -1,4 +1,4 @@
-class RemoveUserFromReviews < ActiveRecord::Migration[5.2]
+class RemoveUserFromReview < ActiveRecord::Migration[5.2]
   def change
     remove_reference :reviews, :user, foreign_key: true
   end
