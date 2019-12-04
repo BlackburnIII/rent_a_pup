c30d1449441f4d790ea3003d45d0448503aff978
Lee Gabardi <lrgabardi@gmail.com>
2019-12-04 10:38:00 +0100

    continue working on edit update

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'pages#home'
+
+  get "puppies/:id/edit", to: "puppies#edit"
+
+  patch "puppies/:id", to: "puppies#update"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
